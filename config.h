ed9db5e5377b4e6bd2908f6b5abe2d24f582a9c4
John Doe <jdoe-fedora@foxmail.com>
2017-06-30 20:43:16 +0800

    minor adjustments

--- config.h
+++ config.h
@@ -24,18 +24,17 @@ typedef uint32_t		 		osCounter_t;
 typedef uint8_t 				osByte_t;
 typedef void* 					osCode_t;
 
-
 /* miscellaneous */
-#define OS_PRIO_LOWEST			UINT32_MAX /* largest value of osCounter_t */
+#define OS_PRIO_LOWEST					UINT32_MAX /* largest value of osCounter_t */
 #define MEMORY_ALIGNMENT 				(4)
-#define IDLE_THREAD_STACK_SIZE 			(128)
+#define IDLE_THREAD_STACK_SIZE 		(128)
 #define TIMER_THREAD_STACK_SIZE 		(1024)
 #define OS_TICK_HANDLER_NAME 			SysTick_Handler
 #define OS_RESET_HANDLER_NAME			Reset_Handler
 #define MAIN_STACK_SIZE				(1024)
 
 /* compiler */
-#define INLINE 			static __inline__ __attribute__((always_inline))
+#define INLINE 		static __inline__ __attribute__((always_inline))
 #define NORETURN		__attribute__((noreturn))
 #define INTERRUPT		__attribute__((interrupt))
 
